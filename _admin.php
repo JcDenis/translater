d5ba4e12981ae7327a45fc163bd1b8128c560041
JcDenis <contact@jcdenis.fr>
2021-08-19 23:11:14 +0200

    update license

--- _admin.php
+++ _admin.php
@@ -3,7 +3,7 @@
 #
 # This file is part of translater, a plugin for Dotclear 2.
 # 
-# Copyright (c) 2009-2016 Jean-Christian Denis and contributors
+# Copyright (c) 2009-2021 Jean-Christian Denis and contributors
 # 
 # Licensed under the GPL version 2.0 license.
 # A copy of this license is available in LICENSE file or at
--- inc/class.translater.proposals.php
+++ inc/class.translater.proposals.php
@@ -3,8 +3,8 @@
 #
 # This file is part of translater, a plugin for Dotclear 2.
 # 
-# Copyright (c) 2009-2016 Jean-Christian Denis and contributors
-#
+# Copyright (c) 2009-2021 Jean-Christian Denis and contributors
+# 
 # Licensed under the GPL version 2.0 license.
 # A copy of this license is available in LICENSE file or at
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
